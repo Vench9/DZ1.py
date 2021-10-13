b4baba7b69a72acf921b99c9e37afb01e18043f4
Vench_97 <vnuk-vanya@mail.ru>
2021-10-13 14:53:38 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,12 @@
+var_int = 10
+var_float = 8.4
+var_str = 'No'
+var_int = var_int * 3.5
+big_int = var_int
+var_float = var_float-1
+big_int = var_float
+print(big_int)
+print(var_int / var_float)
+print( big_int / var_float)
+var_str = var_str * 2 + "Yes" * 3
+print(var_str)
